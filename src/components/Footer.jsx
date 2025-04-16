ab47d746dd4d89a7d42362a86f0b7d92f8a0659d
AldoMariano <aldomay1310@gmail.com>
2025-04-15 21:45:22 -0600

    si

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer className="bg-brand-dark text-white text-center py-4 mt-12">
-      <p>© 2025 GreenStore. Todos los derechos reservados.</p>
+      <p>© 2025 BARTOSTYLE. Todos los derechos reservados.</p>
     </footer>
   );
 }
