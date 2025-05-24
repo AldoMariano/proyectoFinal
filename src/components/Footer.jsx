a9764ffc59707dadc0517fcbc940751f428f9983
AldoMariano <161859574+AldoMariano@users.noreply.github.com>
2025-05-24 05:46:58 +0000

    proyectoo

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-brand-dark text-white text-center py-4 mt-12">
-      <p>© 2025 BARTOSTYLE. Todos los derechos reservados.</p>
-    </footer>
-  );
-}
