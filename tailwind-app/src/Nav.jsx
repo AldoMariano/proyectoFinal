4ad9cb18937aee290902efeb25fdc501489e7876
AldoMariano <161859574+AldoMariano@users.noreply.github.com>
2025-05-26 16:09:36 +0000

    barto

--- tailwind-app/src/Nav.jsx
+++ tailwind-app/src/Nav.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import reactLogo from './assets/BartoStylee.png';
+import reactLogo from './assets/simp.gif';
 
 const navigation = [
   { title: "Casa", href: "/" },
